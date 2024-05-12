5ef14866a74be53c6742d6ab79928f115a95ff1c
akajtar <akajtar58@gmail.com>
2024-05-12 11:21:40 +0200

    photo gallery shows every photo

--- geo_gallery/lib/screens/photo_gallery_screen.dart
+++ geo_gallery/lib/screens/photo_gallery_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'dart:typed_data';
-import 'dart:io';
 
 class PhotoGalleryScreen extends StatefulWidget {
   @override
@@ -9,7 +8,12 @@ class PhotoGalleryScreen extends StatefulWidget {
 }
 
 class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
+  List<AssetEntity> _mediaList = [];
   List<Map<String, dynamic>> _images = [];
+  bool _loading = false;
+  int _currentPage = 0;
+  int _pageSize = 20;
+  bool _allLoaded = false;
 
   @override
   void initState() {
@@ -18,43 +22,62 @@ class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
   }
 
   Future<void> _loadAndDisplayImages() async {
+    setState(() {
+      _loading = true;
+    });
+
     try {
-      final List<AssetEntity> mediaList = await PhotoManager.getAssetPathList(
-        type: RequestType.image,
-      ).then((value) => value[0].getAssetListRange(
-            start: 0,
-            end: 1000000,
-          ));
+      final List<AssetEntity> mediaList = await _fetchMedia();
+      if (mediaList.isEmpty) {
+        _allLoaded = true;
+      }
+      _mediaList.addAll(mediaList);
 
       for (AssetEntity media in mediaList) {
         await _displayImage(media);
       }
     } catch (e) {
-      print('Error loading images: $e');
+      print('Error loading images:  $e');
+    } finally {
+      setState(() {
+        _loading = false;
+      });
     }
+  }
+
+  Future<List<AssetEntity>> _fetchMedia() async {
+    final paths = await PhotoManager.getAssetPathList(type: RequestType.image);
+    final mediaList = await paths[0].getAssetListRange(
+      start: _currentPage * _pageSize,
+      end: (_currentPage + 1) * _pageSize,
+    );
+    return mediaList;
   }
 
   Future<void> _displayImage(AssetEntity asset) async {
     try {
-      double? latitude = asset.latitude;
-      double? longitude = asset.longitude;
-      String? createDateTime = asset.createDateTime?.toString();
+      final latitude = asset.latitude;
+      final longitude = asset.longitude;
+      final createDateTime = asset.createDateTime?.toString();
 
       if (latitude != null && longitude != null) {
-        File? file = await asset.file;
-        if (file != null) {
-          Uint8List bytes = await file.readAsBytes();
-          Map<String, dynamic> imageData = {
-            'bytes': bytes,
-            'date': createDateTime,
-          };
+        final bytes = await asset.originBytes;
+        if (bytes != null) {
+          final imageData = {'bytes': bytes, 'date': createDateTime};
           setState(() {
             _images.add(imageData);
           });
         }
       }
     } catch (e) {
-      print('Error displaying image: $e');
+      print('Error loading images:  $e');
+    }
+  }
+
+  void _loadMoreImages() {
+    if (!_loading && !_allLoaded) {
+      _currentPage++;
+      _loadAndDisplayImages();
     }
   }
 
@@ -64,43 +87,53 @@ class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
       appBar: AppBar(
         title: Text('Photo Gallery'),
       ),
-      body: _buildImageGrid(),
+      body: _loading && _images.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : _buildImageList(),
     );
   }
 
-  Widget _buildImageGrid() {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 4.0,
-        mainAxisSpacing: 4.0,
-      ),
-      itemCount: _images.length,
-      itemBuilder: (context, index) {
-        return _buildImageWidget(_images[index]);
-      },
-    );
+  Widget _buildImageList() {
+    return _images.isEmpty
+        ? Center(
+            child: Text('No images to display'),
+          )
+        : NotificationListener<ScrollNotification>(
+            onNotification: (scrollInfo) {
+              if (!_loading &&
+                  !_allLoaded &&
+                  scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent) {
+                _loadMoreImages();
+              }
+              return true;
+            },
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
+              ),
+              itemCount: _images.length,
+              itemBuilder: (context, index) {
+                return _buildImageWidget(_images[index]);
+              },
+            ),
+          );
   }
 
   Widget _buildImageWidget(Map<String, dynamic> imageData) {
-    Uint8List bytes = imageData['bytes'];
-    String? createDateTime = imageData['date'];
+    final bytes = imageData['bytes'];
+    final createDateTime = imageData['date'];
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: Image.memory(
-            bytes,
-            fit: BoxFit.cover,
-          ),
-        ),
-        SizedBox(height: 8.0),
-        Text(
-          'Created: ${createDateTime ?? ''}',
-          style: TextStyle(fontSize: 12.0),
-        ),
-      ],
-    );
+    return Card(
+    elevation: 4.0,
+    child: Image.memory(
+      bytes,
+      fit: BoxFit.cover,
+    ),
+  );
   }
 }
